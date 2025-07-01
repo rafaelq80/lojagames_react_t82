93313d1f84a3c69a7ac64c3bea985e99ed179d7e
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-07-01 13:06:00 -0300

    💥 Consumo do Recurso Categoria - Sem Recursividade

--- src/components/categorias/deletarcategorias/DeletarCategoria.tsx
+++ src/components/categorias/deletarcategorias/DeletarCategoria.tsx
@@ -1,88 +1,89 @@
-﻿import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import { ClipLoader } from "react-spinners"
-import Categoria from "../../../models/Categoria"
-import { deletar, listar } from "../../../services/Service"
+﻿import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { ClipLoader } from 'react-spinners'
+import Categoria from '../../../models/Categoria'
+import { deletar, listar } from '../../../services/Service'
 
 function DeletarCategoria() {
+	const navigate = useNavigate()
 
-    const navigate = useNavigate()
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
 
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
+	const { id } = useParams<{ id: string }>()
 
-    const { id } = useParams<{ id: string }>();
+	async function buscarPorId(id: string) {
+		try {
+			await listar(`/categorias/${id}`, setCategoria)
+		} catch (error: any) {
+			alert('Tema não encontrado!')
+		}
+	}
 
-    async function buscarPorId(id: string) {
-        try {
-            await listar(`/categorias/${id}`, setCategoria)
-        } catch (error: any) {
-            alert('Tema não encontrado!')
-            console.error(error)
-        }
-    }
+	useEffect(() => {
+		if (id !== undefined) {
+			buscarPorId(id)
+		}
+	}, [id])
 
-    useEffect(() => {
-        if (id !== undefined) {
-            buscarPorId(id)
-        }
-    }, [id])
+	async function deletarCategoria() {
+		setIsLoading(true)
 
-    async function deletarCategoria() {
-        setIsLoading(true)
+		try {
+			await deletar(`/categorias/${id}`)
 
-        try {
-            await deletar(`/categorias/${id}`)
+			alert('Categoria apagada com sucesso')
+		} catch (error) {
+			alert('Erro ao apagar a categoria')
+		}
 
-            alert('Categoria apagada com sucesso')
+		setIsLoading(false)
+		retornar()
+	}
 
-        } catch (error) {
-            alert('Erro ao apagar a categoria')
-            console.error(error)
-        }
+	function retornar() {
+		navigate('/categorias')
+	}
 
-        setIsLoading(false)
-        retornar()
-    }
-
-    function retornar() {
-        navigate("/categorias")
-    }
-
-    return (
-        <div className='container w-full max-w-md mx-auto px-4 pt-20 md:pt-6'>
-            <h1 className='text-3xl md:text-4xl text-center py-4'>Deletar Categoria</h1>
-            <p className='text-center font-semibold mb-4 text-base md:text-lg'>
-                Você tem certeza de que deseja apagar a categoria a seguir?</p>
-            <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-                <header
-                    className='py-2 px-4 md:px-6 bg-slate-600 text-white font-bold text-lg md:text-2xl'>
-                    Categoria
-                </header>
-                <p className='p-4 md:p-8 text-xl md:text-3xl bg-white h-full'>{categoria.tipo}</p>
-                <div className="flex flex-row">
-                    <button
-                        className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2 text-base md:text-lg'
-                        onClick={retornar}
-                    >
-                        Não
-                    </button>
-                    <button
-                        className='w-full text-slate-100 bg-teal-600 hover:bg-teal-700 flex items-center justify-center text-base md:text-lg'
-                        onClick={deletarCategoria}
-                    >
-                        {isLoading ?
-                            <ClipLoader
-                            color="#ffffff"
-                            size={24}
-                          />
-                            :
-                            <span>Sim</span>
-                        }
-                    </button>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="container w-1/3 mx-auto">
+			<h1 className="py-4 text-4xl text-center">
+				Deletar Categoria
+			</h1>
+			<p className="mb-4 font-semibold text-center">
+				Você tem certeza de que deseja apagar a categoria a
+				seguir?
+			</p>
+			<div className="flex flex-col justify-between overflow-hidden border rounded-2xl">
+				<header className="px-6 py-2 text-2xl font-bold text-white bg-slate-600">
+					Categoria
+				</header>
+				<p className="h-full p-8 text-3xl bg-white">
+					{categoria.tipo}
+				</p>
+				<div className="flex">
+					<button
+						className="w-full py-2 bg-red-500 text-slate-50 hover:bg-red-600"
+						onClick={retornar}
+					>
+						Não
+					</button>
+					<button
+						className="flex items-center justify-center w-full bg-teal-600 text-slate-50 hover:bg-teal-800"
+						onClick={deletarCategoria}
+					>
+						{isLoading ? (
+							<ClipLoader
+								color="#ffffff"
+								size={24}
+							/>
+						) : (
+							<span>Sim</span>
+						)}
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 export default DeletarCategoria
