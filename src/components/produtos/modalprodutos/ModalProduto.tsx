bf4d53bbddc63049feaad0a2b2362ade3c22fb43
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-11-16 23:33:54 -0300

    Update

--- src/components/produtos/modalprodutos/ModalProduto.tsx
+++ src/components/produtos/modalprodutos/ModalProduto.tsx
@@ -10,7 +10,7 @@ function ModalProduto() {
                 trigger={
                     <button 
                         className='border rounded px-4 py-2 hover:bg-white hover:text-slate-800'>
-                        Nova Postagem
+                        Novo Produto
                     </button>
                 }
                 modal
